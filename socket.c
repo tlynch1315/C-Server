b82bfa140cdef8c97c8dce9f3a1b2d603566ba18
Zac Cendejas <zcendeja@nd.edu>
2017-05-04 13:17:46 -0400

    Socket Installed

--- socket.c
+++ socket.c
@@ -18,19 +18,41 @@
 int
 socket_listen(const char *port)
 {
-    struct addrinfo  hints;
+	struct addrinfo  hints = {
+		.ai_family = AF_UNSPEC,
+		.ai_socktype = SOCK_STREAM,
+		.ai_flags = AI_PASSIVE,
+	};
     struct addrinfo *results;
     int    socket_fd = -1;
 
     /* Lookup server address information */
+	if ((int status = getaddrinfo(NULL, port, &hints, &results)) != 0){
+		fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(status));
+		return -1;
+	}
 
     /* For each server entry, allocate socket and try to connect */
     for (struct addrinfo *p = results; p != NULL && socket_fd < 0; p = p->ai_next) {
-	/* Allocate socket */
-
-	/* Bind socket */
-
+		/* Allocate socket */
+		if ((socket_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0){
+			fprintf(stderr, "socket failed: %s\n", strerror(errno));
+			continue;
+		}
+		/* Bind socket */
+		if (bind(socket_fd, p->ai_addr, p->ai_addrlen) < 0){
+			fprintf(stderr, "bind failed: %s\n", strerror(errno));
+			close(socket_fd);
+			socket_fd = -1;
+			continue;
+		}
     	/* Listen to socket */
+		if (listen(socket_fd, SOMAXCONN) < 0){
+			fprintf(stderr, "listen failed: %s\n", strerror(errno));
+			close(socket_fd);
+			socket_fd = -1;
+			continue;
+		}
     }
 
     freeaddrinfo(results);
