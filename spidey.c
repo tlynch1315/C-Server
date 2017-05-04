4373d7bff67143f0f9d055b60f449ff43fdc9a59
Thomas Michael Lynch <tlynch2@student00.cse.nd.edu>
2017-05-04 19:36:09 -0400

    Complete?

--- spidey.c
+++ spidey.c
@@ -38,14 +38,56 @@ usage(const char *progname, int status)
 int
 main(int argc, char *argv[])
 {
-    int c;
     int sfd;
+    char * temp;
 
     /* Parse command line options */
+    char * PROGRAM_NAME = argv[0];
+    if(argc == 1) usage(PROGRAM_NAME, 1);
+    int argind = 1;
+    while(argind < argc && strlen(argv[argind]) > 1 && argv[argind][0] == '-'){
+        char * arg = argv[argind++];
+        switch (arg[1]){
+            case 'h':
+                usage(PROGRAM_NAME, 0);
+                break;
+            case 'c':
+                temp = argv[argind++];
+                if(streq(temp, "forking"))
+                    ConcurrencyMode = FORKING;
+                else if(streq(temp, "single"))
+                    ConcurrencyMode = SINGLE;
+                else
+                    ConcurrencyMode = UNKNOWN;
+                break;
+            case 'm':
+                MimeTypesPath = argv[argind++];
+                break;
+            case 'M':
+                DefaultMimeType = argv[argind++];
+                break;
+            case 'p':
+                Port = argv[argind++];
+                break;
+            case 'r':
+                RootPath = argv[argind++];
+                break;
+            default:
+                usage(PROGRAM_NAME, 1);
+                break;
+        }
+    }
+                
 
     /* Listen to server socket */
 
+    if((sfd = socket_listen(Port)) < 0){
+        printf("Socket listen failed\n");
+        return EXIT_FAILURE;
+    }
+
     /* Determine real RootPath */
+    RootPath = realpath(RootPath, NULL);
 
     log("Listening on port %s", Port);
     debug("RootPath        = %s", RootPath);
@@ -54,6 +96,20 @@ main(int argc, char *argv[])
     debug("ConcurrencyMode = %s", ConcurrencyMode == SINGLE ? "Single" : "Forking");
 
     /* Start either forking or single HTTP server */
+
+    switch(ConcurrencyMode){
+        case SINGLE:
+            single_server(sfd);
+            break;
+        case FORKING:
+            forking_server(sfd);
+            break;
+        case UNKNOWN:
+            printf("Unknown Concurrency Mode");
+            break;
+    }
+
+
     return EXIT_SUCCESS;
 }
 
